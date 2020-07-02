f8976b288daf45872fdd884394f25b2b581a306f
cacing69 <cacingworm69@gmail.com>
2020-07-02 17:25:50 +0700

    remove primary key

--- model/tester.go
+++ model/tester.go
@@ -2,8 +2,8 @@ package model
 
 type Tester struct {
 	ID    uint   `json:"id" gorm:"primary_key;column:tester_id"`
-	Key   string `json:"key" gorm:"primary_key;column:tester_key"`
-	Value string `json:"value" gorm:"primary_key;column:tester_value"`
+	Key   string `json:"key" gorm:"column:tester_key"`
+	Value string `json:"value" gorm:"column:tester_value"`
 }
 
 func (m Tester) TableName() string {
